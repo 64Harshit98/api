dcca5cfdca4ba7533ca8fa5a63c39ca49136b9b6
64Harshit98 <harshit.singh6498@gmail.com>
2020-06-12 16:49:14 +0530

    User Sign Up with Firebase email and password 🚀

---
+++ src/middlewares/validators/userValidation.js
@@ -0,0 +1,15 @@
+const Joi = require("@hapi/joi");
+const { number } = require("@hapi/joi");
+
+const userRegisterValidation = (data) => {
+	// Define Schema to validate
+	const schema = Joi.object({
+		name: Joi.string().required(),
+		email: Joi.string().min(5).required().email(),
+		password: Joi.string().min(6).required(),
+		phone: Joi.number().min(10),
+	});
+	return schema.validate(data);
+};
+
+module.exports.userRegisterValidation = userRegisterValidation;
--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -16,4 +16,7 @@ MainUserRouter.get("/", (req, res) => {
 	res.status(200).send("In User Router 👤");
 });
 
+MainUserRouter.route("/register")
+	.get(require("./userRegister"))
+	.post(require("./userRegister"));
 module.exports = MainUserRouter;
